af183c2f2933a482927ac9b08f7331913142b2a1
Aaron I Trierweiler <ait530@users.noreply.github.com>
2017-06-27 12:40:02 -0500

    simple mysql connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+// var passport = require('passport');
+// var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 var mysql = require("mysql");
-var inquirer = require("inquirer");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,55 +13,64 @@ var connection = mysql.createConnection({
   // Your password
   password: "",
   database: "share_Closetdb"
-});
 
+});
 
 connection.connect(function(err) {
   if (err) throw err;
-  runSearch();
+  console.log("Connected as id " + connection.threadId);
 });
+
+connection.query("SELECT * FROM dresses", function(err, res) {
+  if (err) throw err;
+  console.log(res);
+});
+
+
+
+
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
 //   credentials (in this case, a token, tokenSecret, and Google profile), and
 //   invoke a callback with a user object.
-passport.use(new GoogleStrategy({
-    consumerKey: GOOGLE_CONSUMER_KEY,
-    consumerSecret: GOOGLE_CONSUMER_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
-  }
-));
+// passport.use(new GoogleStrategy({
+//     consumerKey: GOOGLE_CONSUMER_KEY,
+//     consumerSecret: GOOGLE_CONSUMER_SECRET,
+//     callbackURL: "http://www.example.com/auth/google/callback"
+//   },
+//   function(token, tokenSecret, profile, done) {
+//       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//         return done(err, user);
+//       });
+//   }
+// ));
 
 // GET /auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  The first step in Google authentication will involve redirecting
 //   the user to google.com.  After authorization, Google will redirect the user
 //   back to this application at /auth/google/callback
-app.get('/auth/google',
-  passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' });
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' });
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+// var passport = require('passport');
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-  function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
-  }
-));
+//   function(accessToken, refreshToken, profile, done) {
+//        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//          return done(err, user);
+//        });
+//   }
+// ));
 
