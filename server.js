7fec906589d9b08bfaea493694b908e3fe448295
Aaron I Trierweiler <ait530@users.noreply.github.com>
2017-07-01 12:24:15 -0500

    configuring mongoDB

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-// Include Server Dependencies
+// Server Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
 // Create Instance of Express
 var app = express();
-var PORT = process.env.PORT || 3000; // Sets an initial port. We'll use this later in our listener
+// Sets an initial port. We'll use this later in our listener
+var PORT = process.env.PORT || 3000; 
+
 
 // Run Morgan for Logging
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -16,7 +18,26 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("./public"));
 
+// -------------------------------------------------
+//MONGO DB
 
+//NEED TO ADD MLAB CONFIGURATION!
+// MongoDB configuration (Change this URL to your own DB)
+mongoose.connect("");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose Connection Successful ✔");
+});
+
+// -------------------------------------------------
+
+
+// Express Server Start Verification
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
