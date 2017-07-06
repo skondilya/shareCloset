73ffaec06ddd5c54a78b64e851c52e456ebd956e
Louise Nyambati <louisen1993@gmail.com>
2017-07-05 20:13:32 -0500

    editing files

--- app/app.js
+++ app/app.js
@@ -1,30 +1,190 @@
-// Include the Main React Dependencies
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Header from './components/Header';
+import Products from './components/Products';
+import Pagination from './components/Pagination';
+import Footer from './components/Footer';
+import QuickView from './components/QuickView';
 
-// Grabs the Routes
-var routes = require("./config/routes");
+class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+			products: [],
+			cart: [],
+			totalItems: 0,
+			totalAmount: 0,
+			term: '',
+			category: '',
+			cartBounce: false,
+			quantity : 1,
+			quickViewProduct: {},
+			modalActive: false
+		};
+		this.handleSearch = this.handleSearch.bind(this);
+		this.handleMobileSearch = this.handleMobileSearch.bind(this);
+		this.handleCategory = this.handleCategory.bind(this);
+		this.handleAddToCart = this.handleAddToCart.bind(this);
+		this.sumTotalItems = this.sumTotalItems.bind(this);
+		this.sumTotalAmount = this.sumTotalAmount.bind(this);
+		this.checkProduct = this.checkProduct.bind(this);
+		this.updateQuantity = this.updateQuantity.bind(this);
+		this.handleRemoveProduct = this.handleRemoveProduct.bind(this);
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	}
+	// Fetch Initial Set of Products from external API
+	getProducts(){
+		//For Localhost use the below url
+		//const url = "products.json";
 
-// Renders the contents according to the route page
-// Displays the contents in the div app of index.html
-// Note how ReactDOM takes in two parameters (the contents and the location)
-ReactDOM.render(routes, document.getElementById("app"));
-// Grabs the Routes
-// var Cart = require("./components/cart");
-// var ProductFeed = require("./components/Product-feed");
-// var Product = require("./components/product");
-// // import header from './components/header';
-// // import footer from './components/footer';
+		// For Production use the below url
+		const url="https://quarkbackend.com/getfile/sivadass/products";
 
+		axios.get(url)
+			.then(response => {
+				this.setState({
+					products : response.data
+				})
+			})
+	}
+	componentWillMount(){
+		this.getProducts();
+	}
 
-// ReactDOM.render(
-// // Here we dump all of the components into a single main-container
-// // Again, treat them like they are any other HTML elements.
-//   <div className="main-container">
-//  <Cart />
-//  <ProductFeed />
-//  <Product />
+	// Search by Keyword
+	handleSearch(event){
+		this.setState({term: event.target.value});
+	}
+	// Mobile Search Reset
+	handleMobileSearch(){
+		this.setState({term: ""});
+	}
+	// Filter by Category
+	handleCategory(event){
+		this.setState({category: event.target.value});
+		console.log(this.state.category);
+	}
+	// Add to Cart
+	handleAddToCart(selectedProducts){
+		let cartItem = this.state.cart;
+		let productID = selectedProducts.id;
+		let productQty = selectedProducts.quantity;
+		if(this.checkProduct(productID)){
+			let index = cartItem.findIndex((x => x.id == productID));
+			cartItem[index].quantity = Number(cartItem[index].quantity) + Number(productQty);
+			this.setState({
+				cart: cartItem
+			})
+		} else {
+			cartItem.push(selectedProducts);
+		}
+		this.setState({
+			cart : cartItem,
+			cartBounce: true,
+		});
+		setTimeout(function(){
+             this.setState({cartBounce:false});
+        }.bind(this),1000);
+		this.sumTotalItems(this.state.cart);
+		this.sumTotalAmount(this.state.cart);
+	}
+	handleRemoveProduct(id, e){
+		let cart = this.state.cart;
+		let index = cart.findIndex((x => x.id == id));
+		cart.splice(index, 1);
+		this.setState({
+			cart: cart
+		})
+		this.sumTotalItems(this.state.cart);
+		this.sumTotalAmount(this.state.cart);
+		e.preventDefault();
+	}
+	checkProduct(productID){
+		let cart = this.state.cart;
+		return cart.some(function(item) {
+			return item.id === productID;
+		});
+	}
+	sumTotalItems(){
+        let total = 0;
+        let cart = this.state.cart;
+		total = cart.length;
+		this.setState({
+			totalItems: total
+		})
+    }
+	sumTotalAmount(){
+        let total = 0;
+        let cart = this.state.cart;
+        for (var i=0; i<cart.length; i++) {
+            total += cart[i].price * parseInt(cart[i].quantity);
+        }
+		this.setState({
+			totalAmount: total
+		})
+    }
+	//Update Quantity
+	updateQuantity(qty){
+		console.log("hola!")
+        this.setState({
+            moq: qty
+        })
+	}
+	//Reset Quantity
+	updateQuantity(qty){
+		console.log("hola!")
+        this.setState({
+            quantity: qty
+        })
+	}
+	// Open Modal
+	openModal(product){
+		this.setState({
+			quickViewProduct: product,
+			modalActive: true
+		})
+	}
+	// Close Modal
+	closeModal(){
+		this.setState({
+			modalActive: false
+		})
+	}
 
-//   </div>
-//   , document.getElementById("app")
-// );
+	render(){
+		return(
+			<div className="container">
+				<Header
+					cartBounce={this.state.cartBounce}
+					total={this.state.totalAmount}
+					totalItems={this.state.totalItems}
+					cartItems={this.state.cart}
+					removeProduct={this.handleRemoveProduct}
+					handleSearch={this.handleSearch}
+					handleMobileSearch={this.handleMobileSearch}
+					handleCategory={this.handleCategory}
+					categoryTerm={this.state.category}
+					updateQuantity={this.updateQuantity}
+					productQuantity={this.state.moq}
+				/>
+				<Products
+					productsList={this.state.products}
+					searchTerm={this.state.term}
+					addToCart={this.handleAddToCart}
+					productQuantity={this.state.quantity}
+					updateQuantity={this.updateQuantity}
+					openModal={this.openModal}
+				/>
+				<Footer />
+				<QuickView product={this.state.quickViewProduct} openModal={this.state.modalActive} closeModal={this.closeModal} />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+  	document.getElementById('root')
+);
--- app/components/cart.js
+++ app/components/cart.js
@@ -3,47 +3,47 @@ var React = require("react");
 
 var cart = React.createClass({
 
-	// getInitialState : function(){
-	// 	return {
-	// 		items: [],
-	// 		total : 0,
-	// 		currency : 'USD'
-	// 	}
-	// },
+	getInitialState : function(){
+		return {
+			items: [],
+			total : 0,
+			currency : 'USD'
+		}
+	},
 
-	// addItem : function(e,item){
-	// 	this.state.items.push(item);
-	// 	this.counttotal();
-	// },
+	addItem : function(e,item){
+		this.state.items.push(item);
+		this.counttotal();
+	},
 
-	// removeItem : function(e,itemId){
-	// 	var itemindex;
-	// 	this.state.items.some(function(item,index){
-	// 		if(item.id ==itemId){
-	// 			itemindex = index;
-	// 			return true;
-	// 		}
-	// 	})
-	// 	if(typeof itemId != 'undefined'){
-	// 		this.state.items.splice(itemindex ,1);
-	// 	}
-	// },
-	// countTotal : function(){
-	// 	var total = 0;
-	// 	this.state.items.forEach(function(item , index){
-	// 		total = total + item.price;
-	// 	})
-	// 	this.setstate({total : total})
-	// },
+	removeItem : function(e,itemId){
+		var itemindex;
+		this.state.items.some(function(item,index){
+			if(item.id ==itemId){
+				itemindex = index;
+				return true;
+			}
+		})
+		if(typeof itemId != 'undefined'){
+			this.state.items.splice(itemindex ,1);
+		}
+	},
+	countTotal : function(){
+		var total = 0;
+		this.state.items.forEach(function(item , index){
+			total = total + item.price;
+		})
+		this.setstate({total : total})
+	},
 
 	render : function(){
-		// var items = this.state.items.map(function(item){
+		var items = this.state.items.map(function(item){
 		return (
 				<p>Hello World!</p>
 
 
 		)
-	
+
 	}
 });
 
