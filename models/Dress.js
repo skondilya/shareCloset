f00b9dba272e1abf172f882f4c449c44fca17b7b
skondilya <skondilya@gmail.com>
2017-07-12 20:00:27 -0500

    updating the models functions

--- models/Dress.js
+++ models/Dress.js
@@ -25,7 +25,41 @@ var DressSchema = new Schema({
 });
 
 // Creates the model 
-var Dress = mongoose.model("Dress", DressSchema);
+const Dress = module.exports = mongoose.model("Dress", DressSchema);
 
+// Get Dresses
+module.exports.getDresses = (callback, limit) => {
+  Dress.find(callback).limit(limit);
+}
+
+// Get Dress
+module.exports.getDressById = (id, callback) => {
+  Dress.findById(id, callback);
+}
+
+// Add Dress
+module.exports.addDress = (dress, callback) => {
+  Dress.create(dress, callback);
+}
+
+// Update Dress
+module.exports.updateDress = (id, dress, options, callback) => {
+  var query = {_id: id};
+  var update = {
+    user_name: dress.user_name,
+    user_nameID: dress.user_nameID,
+    color: dress.color,
+    size: dress.size,
+    year: dress.year,
+    price: dress.price
+  }
+  Dress.findOneAndUpdate(query, update, options, callback);
+}
+
+// Delete Book
+module.exports.removeDress = (id, callback) => {
+  var query = {_id: id};
+  Dress.remove(query, callback);
+}
 // Exports for use elsewhere
 module.exports = Dress;
